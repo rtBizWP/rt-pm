5ecb2c1741cfcb6d610901a099b717eebf134529
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-10-06 18:48:41 +0530

    chnaged menu order

--- app/buddypress-components/rt-pm-componet/bp-pm/class-rt-pm-bp-pm-loader.php
+++ app/buddypress-components/rt-pm-componet/bp-pm/class-rt-pm-bp-pm-loader.php
@@ -105,7 +105,7 @@ if ( !class_exists( 'RT_PM_Bp_PM_Loader' ) ) {
 			$main_nav = array(
 				'name' 		      => __( 'PM' ),
 				'slug' 		      => $this->id .'/projects',
-				'position' 	      => 80,
+				'position' 	      => 90,
 				'screen_function'     => 'bp_pm_projects',
 				'default_subnav_slug' => 'projects',
 			);
