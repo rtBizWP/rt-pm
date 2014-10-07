3710db459081688816b3cb02560c5be13cca438a
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-10-07 23:14:22 +0530

    fixed the add task button

--- app/buddypress-components/rt-pm-componet/class-rt-pm-bp-pm-project.php
+++ app/buddypress-components/rt-pm-componet/class-rt-pm-bp-pm-project.php
@@ -571,11 +571,11 @@ if( !class_exists( 'Rt_PM_Bp_PM_Project' ) ) {
 					}
 				?>
 				<div class="row">
-					<div class="large-10 columns">
+					<div class="large-8 columns">
 						<h4><?php _e( 'Tasks', RT_PM_TEXT_DOMAIN ) ?></h4>
 					</div>
-					<div class="large-2 columns">
-						<button class="right mybutton add-task" type="button" ><?php _e($btntitle); ?></button>
+					<div class="large-4 columns">
+						<button class="right mybutton add-task" type="button" ><?php _e( $btntitle ); ?></button>
 					</div>
 
 				</div>
