7f9075c14faa481ea4758e2bb9c33f29b26a6229
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-10-14 19:53:20 +0530

    added foundation min css

--- app/buddypress-components/rt-pm-componet/class-rt-pm-bp-pm-frontend.php
+++ app/buddypress-components/rt-pm-componet/class-rt-pm-bp-pm-frontend.php
@@ -55,8 +55,9 @@ if( !class_exists( 'Rt_PM_Bp_PM_Frontend' ) ) {
                     wp_enqueue_script( 'jquery-ui-accordion' );
                 }
 
-				wp_enqueue_media(); // Enqueues all scripts, styles, settings, and templates necessary to use all media JavaScript APIs. 
-
+				wp_enqueue_media(); // Enqueues all scripts, styles, settings, and templates necessary to use all media JavaScript APIs.
+				 
+				wp_enqueue_style( 'foundation-css', RT_PM_URL . 'app/assets/css/foundation.min.css' , array('buddyboss-wp-frontend') );
                 wp_enqueue_style('foundation-icon-general-css', RT_PM_URL . 'app/assets/css/general_foundicons.css', false, "", 'all');
                 wp_enqueue_style('foundation-icon-general-ie-css', RT_PM_URL . 'app/assets/css/general_foundicons_ie7.css', false, "", 'all');
                 wp_enqueue_style('foundation-icon-social-css', RT_PM_URL . 'app/assets/css/social_foundicons.css', false, "", 'all');
