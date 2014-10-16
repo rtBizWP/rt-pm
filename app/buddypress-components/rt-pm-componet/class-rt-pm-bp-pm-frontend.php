64624acbedf739f28e07fdae1aff02fe9085daad
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-10-16 15:06:33 +0530

    updated foundation to latest 3rd time

--- app/buddypress-components/rt-pm-componet/class-rt-pm-bp-pm-frontend.php
+++ app/buddypress-components/rt-pm-componet/class-rt-pm-bp-pm-frontend.php
@@ -62,7 +62,7 @@ if( !class_exists( 'Rt_PM_Bp_PM_Frontend' ) ) {
                 wp_enqueue_style('foundation-icon-social-css', RT_PM_URL . 'app/assets/css/social_foundicons.css', false, "", 'all');
                 wp_enqueue_style('foundation-icon-social-ie-css', RT_PM_URL . 'app/assets/css/social_foundicons_ie7.css', false, "", 'all');
                 wp_enqueue_style('foundation-normalize', RT_PM_URL . 'app/assets/css/legacy_normalize.css', false, '', 'all');
-                //wp_enqueue_style('foundation-legacy-css', RT_PM_URL . 'app/assets/css/legacy_admin.css', false, '', 'all');
+                wp_enqueue_style('foundation-legacy-css', RT_PM_URL . 'app/assets/css/legacy_admin.css', false, '', 'all');
                 wp_enqueue_style('rtpm-admin-css', RT_PM_URL . 'app/assets/css/admin.css', false, '', 'all');
 				wp_enqueue_style( 'foundation-css', RT_PM_URL . 'app/assets/css/foundation.min.css' , array('buddyboss-wp-frontend') );
 				wp_enqueue_style('rtpm-frontend-css', RT_PM_URL . 'app/assets/css/pm-frontend.css', false, RT_PM_VERSION, 'all');
