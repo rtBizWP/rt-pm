64d2086cf78d6fbe6e25601dda601646ef531061
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-10-16 13:25:34 +0530

    changed the grid for time entry header

--- app/buddypress-components/rt-pm-componet/class-rt-pm-bp-pm-project.php
+++ app/buddypress-components/rt-pm-componet/class-rt-pm-bp-pm-project.php
@@ -927,7 +927,7 @@ if( !class_exists( 'Rt_PM_Bp_PM_Project' ) ) {
 							<h4><?php _e( 'Time Entries', RT_PM_TEXT_DOMAIN ) ?></h4>
 						</div>
 						<div class="large-4 columns">
-							<button class="right mybutton add-time-entry" type="button" ><?php _e($btntitle, , RT_PM_TEXT_DOMAIN ); ?></button>
+							<button class="right mybutton add-time-entry" type="button" ><?php _e($btntitle, RT_PM_TEXT_DOMAIN ); ?></button>
 						</div>
 
 					</div>
