8047241c109d9a4774271e4877aa2626f7600cd8
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2014-11-05 12:55:55 +0530

    changed the default settings

--- app/settings/class-rt-pm-settings.php
+++ app/settings/class-rt-pm-settings.php
@@ -93,7 +93,7 @@ if ( ! class_exists( 'Rt_PM_Settings' ) ) {
 				'desc' => 'This label will be used for the Menu Item label for rtPM', // Description of the option
 				'id' => 'menu_label', // Unique ID of the option
 				'type' => 'text', //
-				'default' => __( 'PM' ), // Menu icon for top level menus only
+				'default' => __( 'Projects' ), // Menu icon for top level menus only
 				'example' => '', // An example value for this field, will be displayed in a <code>
 				'livepreview' => '', // jQuery script to update something in the site. For theme customizer only
 			) );
