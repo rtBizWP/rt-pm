c78b1e43196016a0d82a8938ea11ebdb00d7674f
Paresh Radadiya <pareshradadiya@hotmail.com>
2015-12-04 14:23:31 +0530

    Gantt mobile view export buttons fix :bar_chart:

--- app/buddypress-components/rt-pm-componet/templates/pm-ganttchart.php
+++ app/buddypress-components/rt-pm-componet/templates/pm-ganttchart.php
@@ -14,7 +14,7 @@ $project_id = $_REQUEST['rt_project_id'];
         <div class="large-4 columns list-title">
             <h2><?php _e( '#'.get_post_meta(  $project_id, 'rtpm_job_no', true ).' '. get_post_field( 'post_title', $project_id ), RT_PM_TEXT_DOMAIN );?></h2>
         </div>
-        <div class="large-8 columns action-bar">
+        <div class="large-8 columns export-button-bar action-bar">
             <input value="Export to Excel" type="button" onclick='gantt.exportToExcel()'>
             <input value="Export to iCal" type="button" onclick='gantt.exportToICal()'>
             <input value="Export to PDF" type="button" onclick='gantt.exportToPDF()'>
