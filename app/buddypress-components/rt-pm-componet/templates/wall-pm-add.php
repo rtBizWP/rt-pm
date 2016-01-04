53ad37c2f3e5beec4c357ba982cabb5d66eaccad
Chandra Patel <chandra.ddu@gmail.com>
2016-01-04 13:00:05 +0530

    Fixed client and contact links not go to sub domain issue.

--- app/buddypress-components/rt-pm-componet/templates/wall-pm-add.php
+++ app/buddypress-components/rt-pm-componet/templates/wall-pm-add.php
@@ -64,8 +64,12 @@ if( !empty( $results_organization ) ) {
             $logo = '<img src="'.RT_PM_URL.'app/buddypress-components/rt-pm-componet/assets/img/logo-default.png" width="32" height="32" class="attachment-32x32 wp-post-image"/>';
         }
         $email = rt_biz_get_entity_meta( $organization->ID, $rt_pm_project->organization_email_key, true );
-        $company_url = add_query_arg( array( 'id'=>$organization->ID, 'action'=>'edit' ), get_people_root_url().Rt_Bp_People_Loader::$companies_slug) ;
-        if (isset($project_organization) && $project_organization && !empty($project_organization) && in_array($organization->ID, $project_organization)) {
+        //$company_url = add_query_arg( array( 'id'=>$organization->ID, 'action'=>'edit' ), get_people_root_url().Rt_Bp_People_Loader::$companies_slug) ;
+        $url_info = parse_url($organization->guid);
+		$user_info = get_user_by('id', $organization->post_author);
+		$company_url = add_query_arg(array('id' => $organization->ID, 'action' => 'edit' ),
+			$url_info['scheme'] . '://' . $url_info['host'] . '/' . $user_info->user_nicename . '/' . BP_PEOPLE_SLUG . '/' . Rt_Bp_People_Loader::$companies_slug);
+		if (isset($project_organization) && $project_organization && !empty($project_organization) && in_array($organization->ID, $project_organization)) {
             $subProjectOrganizationsHTML .= "<li id='project-org-auth-" . $organization->ID
                 . "' class='contact-list'><div class='row'><div class='column small-2'>" . $logo . '</div><div class="column small-9 vertical-center"><a target="_blank" class="" title="'.$organization->post_title.'" href="'.$company_url.'">'.$organization->post_title.'</a></div>'
                 . "<div class='column small-1 vertical-center'><a class='right' href='#removeProjectOrganization'><i class='foundicon-remove'></i></a>
@@ -108,8 +112,13 @@ $subProjectClientHTML = "";
 if( !empty( $results_client ) ) {
     foreach ( $results_client as $client ) {
         $email = rt_biz_get_entity_meta( $client->ID, $rt_pm_project->contact_email_key, true );
-        $client_url = add_query_arg( array( 'id'=>$client->ID, 'action'=>'edit' ), get_people_root_url().Rt_User_Category::$clients_category_slug ) ;
-        if (isset($project_client) && $project_client && !empty($project_client) && in_array($client->ID, $project_client)) {
+        //$client_url = add_query_arg( array( 'id'=>$client->ID, 'action'=>'edit' ), get_people_root_url().Rt_User_Category::$clients_category_slug ) ;
+		$url_info = parse_url($client->guid);
+		$user_info = get_user_by('id', $client->post_author);
+		$client_url = add_query_arg(array('id' => $client->ID, 'action' => 'edit' ),
+			$url_info['scheme'] . '://' . $url_info['host'] . '/' . $user_info->user_nicename . '/' . BP_PEOPLE_SLUG . '/' . Rt_User_Category::$clients_category_slug);
+        
+		if (isset($project_client) && $project_client && !empty($project_client) && in_array($client->ID, $project_client)) {
             $subProjectClientHTML .= "<li id='project-client-auth-" . $client->ID
                 . "' class='contact-list'><div class='row'><div class='large-2 column'>" . get_avatar($email, 32) . '</div>
                 <div class="column small-9 vertical-center"><a target="_blank" class="" title="'.$client->post_title.'" href="'. $client_url .'">'.$client->post_title.'</a></div>'
